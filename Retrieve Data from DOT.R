15172cf1485232e43b965d39e9a8c9729df0e5f4
Syd Phillips <sydphi@yahoo.com>
2015-04-09 22:22:57 -0500

    Initial Commit

---
+++ Retrieve Data from DOT.R
@@ -0,0 +1,4 @@
+
+url2015-Jan <- "http://www.fhwa.dot.gov/policyinformation/travel_monitoring/15jantvt/15jantvt.xlsx"
+url2014-Dec <- "http://www.fhwa.dot.gov/policyinformation/travel_monitoring/14dectvt/14dectvt.xls"
+url2014-Nov <- "http://www.fhwa.dot.gov/policyinformation/travel_monitoring/14novtvt/14novtvt.xls"
